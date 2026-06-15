33b59ede7bb7626853f35b73136663be72378c99
Prachi Sehrawat <sk6472692@gmail.com>
2026-06-15 15:46:22 +0000

    update

---
+++ 2_functions/prj1.py
@@ -0,0 +1,52 @@
+print("       Welcome to the Computer Quiz Game!      ")
+playing = input("do you want to play? (yes/no): ").lower()
+
+if playing != "yes":
+    print("Okay, Maybe next time!")
+else:
+    print("Great! Let's play")
+
+score = 0
+
+# Question 1
+answer = input("What does CPU stand for? ").lower()
+
+if answer == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 2
+answer = input("What does GPU stand for? ").lower()
+
+if answer == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 3
+answer = input("What does RAM stand for? ").lower()
+
+if answer == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 4
+answer = input("What does ROM stand for? ").lower()
+
+if answer == "read only memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+total_ques = 4
+perc = (score / total_ques) * 100
+
+print("     Quiz Finished      ")
+print("Your", score, "questions are correct!")
+print("You scored", perc , "%")
