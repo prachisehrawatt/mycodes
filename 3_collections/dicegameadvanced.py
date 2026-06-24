5e8e7a0a28101b5c4bc21e1f016616477fbd7dae
Prachi Sehrawat <sk6472692@gmail.com>
2026-06-24 16:56:27 +0000

    update

---
+++ 3_collections/dicegameadvanced.py
@@ -0,0 +1,42 @@
+import random
+from itertools import accumulate
+
+def roll_dice():
+    return random.randint(1, 6)
+n = int(input("Enter number of players: "))
+user = []
+
+for i in range(n):
+    user.append(input(f"Enter Player {i} Name: "))
+
+user.append("System")
+
+scores = [[] for i in range(len(user))]
+
+while True:
+
+    for i in range(len(user)):
+        print(f"{user[i]}'s Turn")
+        roll = roll_dice()
+        print("Rolled:", roll)
+
+        scores[i].append(roll)
+
+        if roll == 6:
+            print("Bonus Chance!")
+            roll = roll_dice()
+            print("Rolled Again:", roll)
+            scores[i].append(roll)
+
+        total = list(accumulate(scores[i]))[-1]
+
+        print("Total Score:", total)
+
+        if total >= 50:
+            print(f"{user[i]} Wins the Game!")
+            break
+
+    print("=====Scoreboard=====")
+    for i in range(len(user)):
+        total = list(accumulate(scores[i]))[-1]
+        print(user[i], ":", total)
